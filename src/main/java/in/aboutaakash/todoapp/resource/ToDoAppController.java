8f6bcdc8f9b51e237d9ce66e92bd6d28d45861a0
aakashverma1124 <info.aakash11@gmail.com>
2020-05-20 17:54:00 +0530

    Resolved CORS Error

--- src/main/java/in/aboutaakash/todoapp/resource/ToDoAppController.java
+++ src/main/java/in/aboutaakash/todoapp/resource/ToDoAppController.java
@@ -1,11 +1,13 @@
 package in.aboutaakash.todoapp.resource;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/to-do-app")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ToDoAppController {
 
 	
